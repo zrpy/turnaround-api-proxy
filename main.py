bff2bdbf92bfc6cd2adea8e9b7eb083969f9bb8d
zrpy <91083604+zrpy@users.noreply.github.com>
2024-10-06 13:10:03 +0900

    Update main.py

--- main.py
+++ main.py
@@ -18,7 +18,8 @@ async def solve():
     url = json_data["url"]
     proxy = json_data.get("proxy","")
     async with playwright.async_api.async_playwright() as p:
-        s = await solver.Solver(p,proxy=proxy,headless=True)
+        s = solver.Solver(p,proxy=proxy,headless=True)
+        await s.start_browser(p)
         token = await s.solve(url, sitekey, invisible)
         await s.terminate()
         return (await make_response(token))
